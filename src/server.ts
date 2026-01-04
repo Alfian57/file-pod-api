294f1b0d176869a94201ac910a42f5f7af2c4471
Alfian57 <alfiangadingsaputra@gmail.com>
2026-01-04 16:17:12 +0700

    chore: update favicon and swagger ui configuration

--- src/server.ts
+++ src/server.ts
@@ -25,6 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
 app.use(helmet());
 app.use(rateLimiter);
+app.use(express.static("src/common/public"));
 
 // Request logging
 app.use(requestLogger);
